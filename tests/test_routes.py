3619a342dee088ee1699d7e32e4f8ea3ca1d47a3
nbhagat3136 <85042602+nbhagat3136@users.noreply.github.com>
2021-07-03 00:09:42 -0400

    New Order Creation (#20)
    New Customer Order Creation

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,8 +12,9 @@ import logging
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 from flask_api import status  # HTTP Status Codes
-from service.models import db
-from service.routes import app, init_db
+from service.model import db
+from service.routes import app
+from unittest.mock import MagicMock
 
 ######################################################################
 #  T E S T   C A S E S
@@ -47,3 +48,26 @@ class TestYourResourceServer(TestCase):
         """ Test index call """
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+    
+    def test_create_order(self):
+        """Test that order is created."""
+        def set_order_id(order):
+            order.order_id = 1  # order_id is assigned by SQLAlchemy in real function.
+        db.session.add = MagicMock(return_value=None, side_effect=set_order_id)
+        db.session.commit = MagicMock(return_value=None)
+        resp = self.app.post('/orders', json={
+            "customer_id": 15,
+            "address_line1": "123 Lost Road Apt 420",
+            "city": "Jersey City",
+            "state": "NJ",
+            "zip_code": 12345})
+        db.session.commit.assert_called()
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+    
+    def test_create_order_error(self):
+        """Test that invalid content type are ignored."""
+        resp = self.app.post(
+            '/orders', data='Non JSON data type',
+            content_type="application/x-www-form-urlencoded")
+        self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
