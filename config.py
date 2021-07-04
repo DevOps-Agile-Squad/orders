6f18ef220c24280ce3d1d12e71516c73d5bbb996
Narmada Bhagat <narmada.bhagat@gmail.com>
2021-07-03 23:16:44 -0400

    Reverting conflict

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ Source: https://github.com/nyu-devops/project-template/blob/master/config.py
 import os
 
 # Get configuration from environment
-DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///../development.db")
+DATABASE_URI = os.getenv("DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres")
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
