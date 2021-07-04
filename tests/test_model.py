b76ff0ef8f445827ede1d7d71ae96629e2ac3033
Narmada Bhagat <narmada.bhagat@gmail.com>
2021-07-04 04:05:50 +0000

    Updating change

--- tests/test_model.py
+++ tests/test_model.py
@@ -124,7 +124,29 @@ class TestItemModel(unittest.TestCase):
 ######################################################################
 class TestCustomerOrderModel(unittest.TestCase):
     """ Test Cases for YourResourceModel Model """
+    @classmethod
+    def setUpClass(cls):
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        OrderBase.init_db(app)
 
+    @classmethod
+    def tearDownClass(cls):
+        """ This runs once after the entire test suite """
+        db.session.close()
+
+    def setUp(self):
+        """ This runs before each test """
+        db.drop_all()  # clean up the last tests
+        db.create_all()  # make our sqlalchemy tables
+
+    def tearDown(self):
+        """ This runs after each test """
+        db.session.remove()
+        db.drop_all()
 
     ######################################################################
     #  T E S T   C A S E S
