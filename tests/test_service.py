b3e703b658830ded9a6c30a537cca3499efb10cf
Joanne Wu <joanne@joannes-air.lan>
2021-07-07 17:11:52 -0400

    let root url to list all the available urls and methods and write corresponding tests

--- tests/test_service.py
+++ tests/test_service.py
@@ -95,12 +95,12 @@ class TestCustomerOrderServer(unittest.TestCase):  # pylint: disable=too-many-pu
             orders.append(test_order)
         return orders
 
-    # def test_index(self):
-    #     """Test the Home Page"""
-    #     resp = self.app.get("/")
-    #     self.assertEqual(resp.status_code, status.HTTP_200_OK)
-    #     data = resp.get_json()
-    #     self.assertEqual(data["name"], "Order Demo REST API Service")
+    def test_index(self):
+        """Test the Home Page"""
+        resp = self.app.get("/")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], "Order Demo REST API Service")
 
     def test_get_orders_list(self):
         """Get a list of orders"""
