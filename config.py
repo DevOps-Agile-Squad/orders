026c470ff2410d42cf71fd68417df6881d0b4a7d
JoanneWooW <mw4708@nyu.edu>
2021-07-18 14:25:23 -0400

    update config.py to overwrite db url to elephant instance if the app is running on cloud

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import json
 
 # Get configuration from environment
 DATABASE_URI = os.getenv(
@@ -9,6 +10,11 @@ DATABASE_URI = os.getenv(
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# override if we are running in Cloud Foundry
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 
 # Secret for session management
 SECRET_KEY = os.getenv("SECRET_KEY", "sup3r-s3cr3t")
