b0ca2fcbda024e6b8b36a3a91d50b4849b6fe566
JoanneWooW <mw4708@nyu.edu>
2021-07-18 14:31:07 -0400

    fix typo: update config.py to put 'override' before 'Configure SQLAlchemy'

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
 )
 
-# Configure SQLAlchemy
-SQLALCHEMY_DATABASE_URI = DATABASE_URI
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 # override if we are running in Cloud Foundry
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.environ['VCAP_SERVICES'])
     DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+
+# Configure SQLAlchemy
+SQLALCHEMY_DATABASE_URI = DATABASE_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Secret for session management
 SECRET_KEY = os.getenv("SECRET_KEY", "sup3r-s3cr3t")
