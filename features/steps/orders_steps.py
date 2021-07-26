2b863a98e81000a716380ad5e1192f85c398d949
Philip Kutlesa <philip.kutlesa@gmail.com>
2021-07-26 03:48:54 +0000

    added sample data and scenario for testing the running server

--- features/steps/orders_steps.py
+++ features/steps/orders_steps.py
@@ -1,7 +1,7 @@
 import json
 import requests
 from behave import *
-from compare import expect 
+from compare import expect, ensure 
 
 """
 Order Steps
@@ -9,3 +9,27 @@ Steps file for Order.feature
 For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """ 
+
+@given(u'the following orders')
+def step_impl(context):
+    """ Delete all Orders and load new ones """
+    headers = {'Content-Type': 'application/json'}
+    # list all of the orders and delete them one by one
+    context.resp = requests.get(context.base_url + '/orders', headers=headers)
+    expect(context.resp.status_code).to_equal(200)
+    for order in context.resp.json():
+        context.resp = requests.delete(context.base_url + '/orders/' + str(order["id"]), headers=headers)
+        expect(context.resp.status_code).to_equal(204)
+    
+    # load the database with new pets
+    create_url = context.base_url + '/orders'
+    for row in context.table:
+        data = {
+            "customer_id": row['customer_id'],
+            "address": row['address'],
+            "status": row['status'],
+            "items": list()
+            }
+        payload = json.dumps(data)
+        context.resp = requests.post(create_url, data=payload, headers=headers)
+        expect(context.resp.status_code).to_equal(201)
---
+++ features/steps/web_steps.py
@@ -0,0 +1,53 @@
+######################################################################
+# Copyright 2016, 2021 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+
+"""
+Web Steps
+Steps file for web interactions with Silenium
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
+import logging
+from behave import when, then
+from compare import expect, ensure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions
+
+@given(u'The server is running')
+def step_impl(context):
+    context.base_url = os.getenv(
+        'BASE_URL',
+        'http://localhost:5000'
+    )
+    context.resp = requests.get(context.base_url + '/')
+    assert context.resp.status_code == 200
+
+@when(u'I visit the "Home Page"')
+def step_impl(context):
+    """ Make a call to the base URL """
+    context.driver.get(context.base_url)
+
+@then('I should see "{message}" in the title')
+def step_impl(context, message):
+    """ Check the document title for a message """
+    expect(context.driver.title).to_contain(message)
+
+@then('I should not see "{message}"')
+def step_impl(context, message):
+    error_msg = "I should not see '%s' in '%s'" % (message, context.resp.text)
+    ensure(message in context.resp.text, False, error_msg)
