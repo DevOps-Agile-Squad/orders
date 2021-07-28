1830257614ea6720293746713cb53284f7708f49
Yaowei Zong <luke0zong@gmail.com>
2021-07-27 20:19:15 -0400

    Add Scenario: Create a new Order

--- features/orders.feature
+++ features/orders.feature
@@ -1,26 +1,43 @@
 Feature: The orders service back-end
-    As an E-Commerce Store Owner  
+    As an E-Commerce Store Owner
     I need a RESTful orders service
-    So that I can manage customer orders 
+    So that I can manage customer orders
 
-Background: 
-    Given the following orders
-        | id         | customer_id | address         | status           | 
-        | 1          | 1           | 101 king st     | Completed        |         
-        | 2          | 2           | 102 king st     | Processing       |       
-        | 3          | 3           | 103 king st     | Processing       |   
+    Background:
+        Given the following orders
+            | id | customer_id | address     | status     |
+            | 1  | 1           | 101 king st | Completed  |
+            | 2  | 2           | 102 king st | Processing |
+            | 3  | 3           | 103 king st | Processing |
 
-    Given the following items
-        | order_id_index    | item_name       | quantity       | price       | 
-        | 0                 | keyboard        | 1              | 35          |
-        | 0                 | frisbee         | 2              | 10          |
-        | 2                 | kite            | 1              | 20          |    
+        Given the following items
+            | order_id_index | item_name | quantity | price |
+            | 0              | keyboard  | 1        | 35    |
+            | 0              | frisbee   | 2        | 10    |
+            | 2              | kite      | 1        | 20    |
 
-Scenario: The server is running 
-    When I visit the "Home Page"
-    Then I should see "Orders RESTful Service" in the title
-    And I should not see "404 Not Found"
+    Scenario: The server is running
+        When I visit the "Home Page"
+        Then I should see "Orders RESTful Service" in the title
+        And I should not see "404 Not Found"
 
+    Scenario: Create a new Order
+        When I visit the "Home Page"
+        And I set the "Customer_ID" to "4869"
+        And I set the "Address" to "221B Baker Street"
+        And I select "Processing" in the "Status" dropdown
+        And I press the "Create" button
+        Then I should see the message "Success"
+        When I copy the "Order_ID" field
+        And I press the "Clear" button
+        Then the "Order_ID" field should be empty
+        And the "Customer_ID" field should be empty
+        And the "Address" field should be empty
+        When I paste the "Order_ID" field
+        And I press the "Retrieve" button
+        Then I should see "4869" in the "Customer_ID" field
+        And I should see "221B Baker Street" in the "Address" field
+        And I should see "Processing" in the "Status" dropdown
 
 
 
--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -28,6 +28,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions
 
+
 @given(u'The server is running')
 def step_impl(context):
     context.base_url = os.getenv(
@@ -37,17 +38,119 @@ def step_impl(context):
     context.resp = requests.get(context.base_url + '/')
     assert context.resp.status_code == 200
 
+
 @when(u'I visit the "Home Page"')
 def step_impl(context):
     """ Make a call to the base URL """
     context.driver.get(context.base_url)
+
 
 @then('I should see "{message}" in the title')
 def step_impl(context, message):
     """ Check the document title for a message """
     expect(context.driver.title).to_contain(message)
 
+
 @then('I should not see "{message}"')
 def step_impl(context, message):
     error_msg = "I should not see '%s' in '%s'" % (message, context.resp.text)
     ensure(message in context.resp.text, False, error_msg)
+
+
+@when('I set the "{element_name}" to "{text_string}"')
+def step_impl(context, element_name, text_string):
+    element_id = element_name.lower()
+    element = context.driver.find_element_by_id(element_id)
+    element.clear()
+    element.send_keys(text_string)
+
+
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
+
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    expect(found).to_be(True)
+
+
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    error_msg = "I should not see '%s' in '%s'" % (name, element.text)
+    ensure(name in element.text, False, error_msg)
+
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    expect(found).to_be(True)
+
+
+@when('I select "{text}" in the "{element_name}" dropdown')
+def step_impl(context, text, element_name):
+    element_id = element_name.lower()
+    element = Select(context.driver.find_element_by_id(element_id))
+    element.select_by_visible_text(text)
+
+
+@then('I should see "{text}" in the "{element_name}" dropdown')
+def step_impl(context, text, element_name):
+    element_id = element_name.lower()
+    element = Select(context.driver.find_element_by_id(element_id))
+    expect(element.first_selected_option.text).to_equal(text)
+
+
+@then('the "{element_name}" field should be empty')
+def step_impl(context, element_name):
+    element_id = element_name.lower()
+    element = context.driver.find_element_by_id(element_id)
+    expect(element.get_attribute('value')).to_be(u'')
+
+
+@then('I should see "{text_string}" in the "{element_name}" field')
+def step_impl(context, text_string, element_name):
+    element_id = element_name.lower()
+    found = WebDriverWait(context.driver, context.WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element_value(
+            (By.ID, element_id),
+            text_string
+        )
+    )
+    expect(found).to_be(True)
+
+
+##################################################################
+# These two function simulate copy and paste
+##################################################################
+@when('I copy the "{element_name}" field')
+def step_impl(context, element_name):
+    element_id = element_name.lower()
+    element = WebDriverWait(context.driver, context.WAIT_SECONDS).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+    context.clipboard = element.get_attribute('value')
+    logging.info('Clipboard contains: %s', context.clipboard)
+
+
+@when('I paste the "{element_name}" field')
+def step_impl(context, element_name):
+    element_id = element_name.lower()
+    element = WebDriverWait(context.driver, context.WAIT_SECONDS).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+    element.clear()
+    element.send_keys(context.clipboard)
